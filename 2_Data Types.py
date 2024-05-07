a6d593f03df2d62e6172df4d74675ec5cc828a7e
Anil <anilsolanki1834@gmail.com>
2024-05-07 20:10:52 +0530

    Add 2_Data Types.py File

---
+++ 2_Data Types.py
@@ -0,0 +1,72 @@
+# String Data Type
+def String():
+    x = str("Hello Python")
+    print("String value is ...   ",x)
+    print("\n")
+
+String()
+
+# Integer Data Type
+def Integer():
+    x = int(-456)
+    print("Integer value is ...   ",x)
+    print("\n")
+
+Integer()
+
+# Float Data Type
+def Float():
+    x = float(-456.203)
+    print("Float value is ...   ",x)
+    print("\n")
+    
+Float()
+
+# Complex Tata Type
+def Complex():
+    x = complex(45j)
+    print("Complex value is ...   ",x)
+    print("\n")
+    
+Complex()
+
+# List Data Type
+def List():
+    x = ["Apple","Banana","Cherry"]
+    print("List value value is ...   ",x)
+    #print(x[1])
+    print("\n")
+    
+List()
+
+# Tuple Data Type
+def Tuple():
+    x = tuple(("Apple","Banana","Cherry"))
+    print("Tuple value is ...   ",x)
+    print("\n")
+    
+Tuple()
+
+# Dictonary Data Type 
+def Dictonary():
+    x = dict(name="Anil",age=22)
+    print("Dictonary value is ...   ",x)
+    print("\n")
+    
+Dictonary()
+
+# Set Data Type
+def Set():
+    x = set(("Apple","Banana","Cherry"))
+    print("Set value is ...",x)
+    print("\n")
+    
+Set()
+
+# Boolean Data Type
+def Boolean():
+    x = bool(1)
+    print("Boolean value is ...",x)
+    print("\n")
+    
+Boolean()

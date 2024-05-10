d427c081c95f0f439221d023f8800b15667808b3
Anil <anilsolanki1834@gmail.com>
2024-05-10 21:41:07 +0530

    ADD 4_Conditional Statement file

---
+++ 4_Conditional Statement.py
@@ -0,0 +1,43 @@
+# Conditional Statement or Decision maker
+
+# [1] if
+
+num = 5
+
+print("*** IF Statement ***")
+if num<10:
+    print("True")
+print("\n")
+
+# [2] if-else
+
+print("*** IF-ELSE Statement ***")
+if(num<2):
+    print("True")
+else:
+    print("False")
+print("\n")
+
+# [3] elif
+
+print("*** ELIF Statement ***")
+if(num<2):
+    print("True")
+elif(num>3):
+    print("True")
+else:
+    print("False")
+print("\n")
+
+# [4] Nested If
+
+print("*** NESTED-IF Statement ***")
+if(num>0):
+    print("Positive Number....")
+    if(num<10):
+        print("True, Num is Less than 10...")
+
+
+
+
+    

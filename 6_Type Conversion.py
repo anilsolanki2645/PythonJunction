46a44c056335cd63a731764dfb358eae56e3b65d
Anil <anilsolanki1834@gmail.com>
2024-05-11 23:30:03 +0530

    Add file 6_Type Conversion in python

---
+++ 6_Type Conversion.py
@@ -0,0 +1,36 @@
+
+# int to float convert
+
+x = 10
+ 
+print("x is of type:",type(x))
+ 
+x=float(x)
+
+print("x is ...",type(x))
+
+# float to int convert
+
+x = 10.25
+ 
+print("x is of type:",type(x))
+ 
+x=int(x)
+
+print("x is ...",type(x))
+
+# string to list
+
+x = "hello"
+ 
+print("x is of type:",type(x))
+ 
+x=list(x)
+
+print("x is ...",type(x))
+print(x)
+
+
+
+
+

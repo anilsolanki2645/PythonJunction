ee3e5e0c5cd3835a7125876e31a77de25ecb7d75
Anil <anilsolanki1834@gmail.com>
2024-05-11 23:28:54 +0530

    Add file 5_Loops in python

---
+++ 5_Loops.py
@@ -0,0 +1,41 @@
+# python Loops
+
+# [1] while Loop
+
+a = 0
+b = 5
+
+while a<4:
+    print(a)
+    a=a+1
+print("\n")
+
+# [2] For loop
+
+for i in range(b,10+1):
+    print(i)
+
+ls = ["anil", "kuldip", "jay", "amit"]
+print(type(ls))
+ts=tuple(ls)
+print(type(ts))
+
+print(ls)
+
+print()
+
+for name in ls:
+    print(name)
+
+print()
+
+
+
+
+
+
+
+
+
+
+    

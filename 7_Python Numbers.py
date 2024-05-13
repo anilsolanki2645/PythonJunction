28f7cc09554c6a69694179ab252946be3c221126
Anil <anilsolanki1834@gmail.com>
2024-05-13 21:46:08 +0530

    add 7 Number data type

---
+++ 7_Python Numbers.py
@@ -0,0 +1,36 @@
+# Python Numbers All Methods
+
+#int
+
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
+print("\n")
+
+#float
+
+x = 1.10
+y = 1.0
+z = -35.59
+
+print(type(x))
+print(type(y))
+print(type(z))
+print("\n")
+
+#Complex
+
+x = 3+5j
+y = 5j
+z = -5j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+

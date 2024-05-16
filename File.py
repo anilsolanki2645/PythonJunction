e914c790af6c0659e583bbf158d01bc0a621a916
Anil <anilsolanki1834@gmail.com>
2024-05-16 21:33:51 +0530

    files in python

---
+++ File.py
@@ -0,0 +1,40 @@
+
+#file Handling in python
+
+print("\n----- File Handling in python -----\n")
+
+# read exel file
+
+print("\n----- Read exel File in python -----\n")
+
+f = open("ab.csv","r")
+
+b = f.read()
+
+print(b)
+
+f.close()
+
+# read txt file
+
+print("\n----- Read txt File in python -----\n")
+
+f1 = open("a.txt","r")
+
+b1 = f1.read()
+
+print(b1)
+
+f1.close()
+
+# read csv file
+
+print("\n----- Read csv File in python -----\n")
+
+f = open("ab.csv","r")
+
+b = f.read()
+
+print(b)
+
+f.close()
